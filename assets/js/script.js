64c214f7d4575f3e42ce58b8de4f99b1c8b5550e
crigaux <crigaux057@gmail.com>
2022-07-25 22:58:12 +0200

    type writer

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,43 @@ lightMode.addEventListener('change', () => {
         document.querySelector('.figma').setAttribute('src', "./assets/img/figmaBlack.svg");
     }
 });
+
+// ######################################################
+// #####            Curseur personnalisé            ##### 
+// ######################################################
+
+const customCursor = document.querySelector('.customCursor');
+
+window.addEventListener('mousemove', handleCustomCursor);
+
+function handleCustomCursor(e) {
+    customCursor.style.transform = `translate(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%))`
+}
+
+// ######################################################
+// #####                Type Writer                ##### 
+// ######################################################
+
+const title = document.querySelector('h1');
+const subtitle = document.querySelector('.subtitle');
+const heroPushLink = document.querySelector('.heroLink');
+const txt = "Moi c'est Claude";
+
+function typewriter(text, index) {
+    if(index > 3) {
+        subtitle.classList.add('active');
+    }
+    if(index > 6) {
+        heroPushLink.classList.add('active');
+    }
+    if(index < text.length) {
+        setTimeout(() => {
+            title.textContent += text[index];
+            typewriter(text, index + 1)
+        }, 200)
+    }
+}
+
+setTimeout(() => {
+    typewriter(txt, 0)
+}, 300)
